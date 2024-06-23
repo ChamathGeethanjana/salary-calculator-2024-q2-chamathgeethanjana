5fc635bccc130236881b27590ee2d8583aae3e32
Chamath Geethanjana <geethanjana.cgs@gmail.com>
2024-06-23 18:55:31 +0530

    refactor: Update page.tsx to use absolute imports and improve code structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,45 @@
 "use client";
 
-import { NumberInput } from "@/components/input.number";
-import { TextButton } from "@/components/text.button";
-import { useAppSelector } from "@/hooks";
-import { selectEarnings } from "@/slices/transactionSlice";
+import { ModalHandler, TextButton, TextInput } from "@/components";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import {
+  removeTransaction,
+  selectDeductions,
+  selectEarnings,
+} from "@/slices/transactionSlice";
+import { Transaction } from "@/types";
 import { useRef, useState } from "react";
-import { ModalHandler } from "../components/model";
 import { AddTransactionModal } from "./components/AddTransactionModal";
+import { TransactionItem } from "./components/TransactionItem";
+import { SalaryCard } from "./components/SalaryCard";
 
 export default function Home() {
-  const [basicSalary, setBasicSalary] = useState(100000);
+  const [basicSalary, setBasicSalary] = useState<number>(0);
   const [type, setType] = useState<"earning" | "deduction">("earning");
+  const [selectedTransaction, setSelectedTransaction] = useState<
+    Transaction | undefined
+  >(undefined);
+
   const modalRef = useRef<ModalHandler>(null);
 
+  const dispatch = useAppDispatch();
+
   const earnigs = useAppSelector(selectEarnings);
+  const deductions = useAppSelector(selectDeductions);
 
   const handaleAddTransaction = (type: "earning" | "deduction") => {
     setType(type);
+    setSelectedTransaction(undefined);
     modalRef.current?.open();
+  };
+
+  const handleEditTransaction = (transaction: Transaction) => {
+    setSelectedTransaction(transaction);
+    modalRef.current?.open();
+  };
+
+  const handleRemoveTransaction = (transaction: Transaction) => {
+    dispatch(removeTransaction(transaction));
   };
 
   return (
@@ -26,38 +48,28 @@ export default function Home() {
         <div className="bg-secondarybg w-full lg:w-7/12 p-5 border rounded-lg  border-gray">
           <h1 className="text-xl font-semibold ">Calculate Your Salary</h1>
           <div className="mt-4 pt-3">
-            <NumberInput
+            <TextInput
+              textInputType="number"
               label="Basic Salary"
               value={basicSalary}
-              onChange={(value) => setBasicSalary(value)}
+              onChange={(value) => setBasicSalary(parseFloat(value))}
             />
           </div>
           <div className="pt-4 ">
             <div className="font-semibold ">
               <h2>Earnings</h2>
             </div>
-            <span className="font-normal text-sm text-gray-500 text-textgray">
+            <span className="font-normal text-sm text-gray-500">
               Allowance, Fixed Allowance, Bonus and etc
             </span>
-            {earnigs.map((earning) => {
-              return (
-                <div
-                  key={earning.id}
-                  className="flex justify-between items-center"
-                >
-                  <div>
-                    <h1>{earning.name}</h1>
-                    <span>{earning.amount}</span>
-                  </div>
-                  <div>
-                    <button className="text-blue hover:opacity-85">Edit</button>
-                    <button className="text-red hover:opacity-85">
-                      Delete
-                    </button>
-                  </div>
-                </div>
-              );
-            })}
+            {earnigs.map((earning) => (
+              <TransactionItem
+                key={earning.id}
+                transaction={earning}
+                onEdit={handleEditTransaction}
+                onRemove={handleRemoveTransaction}
+              />
+            ))}
             <div className="pt-5">
               <TextButton
                 onClick={() => {
@@ -75,6 +87,14 @@ export default function Home() {
             <span className="font-normal text-sm text-gray-500  text-textgray">
               Salary Advance, Loan Deductions, and all
             </span>
+            {deductions.map((deduction) => (
+              <TransactionItem
+                key={deduction.id}
+                transaction={deduction}
+                onEdit={handleEditTransaction}
+                onRemove={handleRemoveTransaction}
+              />
+            ))}
 
             <div className="pt-5">
               <button
@@ -88,12 +108,14 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="border rounded-lg w-full lg:w-5/12 p-5  border-gray">
-          <h1 className="text-xl font-semibold ">Your Salary</h1>
-          {/* <Table basicSalary={basicSalary} /> */}
-        </div>
+        <SalaryCard basicSalary={basicSalary} />
       </div>
-      <AddTransactionModal modalRef={modalRef} type={type} />;
+      <AddTransactionModal
+        modalRef={modalRef}
+        type={type}
+        transaction={selectedTransaction}
+      />
+      ;
     </>
   );
 }
