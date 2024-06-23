dac3ddd708b8c08b6fad5d3c8e4751d2f45becb7
Chamath Geethanjana <geethanjana.cgs@gmail.com>
2024-06-23 23:43:11 +0530

    refactor: Improve TransactionItem component structure and add number formatting

--- src/app/components/TransactionItem.tsx
+++ src/app/components/TransactionItem.tsx
@@ -1,5 +1,6 @@
 import { IconButton } from "@/components";
 import { Transaction } from "@/types";
+import { parseNumberFormat } from "@/utils/number-format";
 import { FC } from "react";
 
 import { MdModeEdit, MdClose, MdCheck } from "react-icons/md";
@@ -16,14 +17,17 @@ export const TransactionItem: FC<TransactionItemProps> = ({
   onEdit,
 }) => {
   return (
-    <div key={transaction.id} className="flex justify-between items-center">
-      <div className="flex">
-        <h1>{transaction.name}</h1>
-        <span>{transaction.amount}</span>
+    <div
+      key={transaction.id}
+      className="flex justify-between items-center my-4"
+    >
+      <div className="flex space-x-2">
+        <h1>{transaction.name}:</h1>
+        <span>{parseNumberFormat(transaction.amount)}</span>
 
         {transaction.type === "earning" && transaction.isEPFandETP && (
-          <span className="text-xs text-gray-500">
-            <MdCheck /> PF/ETF
+          <span className="flex items-center text-xs text-gray-500">
+            <MdCheck className="text-primary" /> EPF/ETF
           </span>
         )}
       </div>
