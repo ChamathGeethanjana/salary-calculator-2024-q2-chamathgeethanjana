b50acd33b9e866f8c8b2178c436ac64f7d1cdc8d
Chamath Geethanjana <geethanjana.cgs@gmail.com>
2024-06-23 18:56:59 +0530

    refactor: Add validation for minimum amount in earning form

--- src/config/validations.ts
+++ src/config/validations.ts
@@ -3,6 +3,8 @@ import * as Yup from "yup";
 export const VALIDATION = {
   earningValidation: Yup.object().shape({
     name: Yup.string().required("Name is required"),
-    amount: Yup.number().required("Amount is required"),
+    amount: Yup.number()
+      .required("Amount is required")
+      .min(1, "Amount must be greater than 0"),
   }),
 };
