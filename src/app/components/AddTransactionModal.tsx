53fee30f5e1de7d800a8ae4d6c926923d8301c30
Chamath Geethanjana <geethanjana.cgs@gmail.com>
2024-06-23 16:45:33 +0530

    chore: Add AddTransactionModal component for adding transactions

---
+++ src/app/components/AddTransactionModal.tsx
@@ -0,0 +1,94 @@
+import { Modal, ModalHandler } from "@/components/model";
+import { FC } from "react";
+import { Transaction } from "@/types";
+import { Formik } from "formik";
+import { Button, TextInput } from "@/components";
+import { VALIDATION } from "@/config";
+import { v4 as uuidv4 } from "uuid";
+import { useAppDispatch } from "@/hooks";
+import { addTransaction } from "@/slices/transactionSlice";
+
+interface AddTransactionModalProps {
+  modalRef: React.RefObject<ModalHandler>;
+  type: "earning" | "deduction";
+}
+
+export const AddTransactionModal: FC<AddTransactionModalProps> = ({
+  modalRef,
+  type,
+}) => {
+  const initialState: Transaction = {
+    id: uuidv4(),
+    name: "",
+    amount: 0,
+    isEPFandETP: false,
+    type: type,
+  };
+
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = (values: Transaction) => {
+    dispatch(addTransaction(values));
+    modalRef.current?.close();
+  };
+
+  const title = type === "earning" ? "Add new Earning" : "Add new Deduction";
+
+  return (
+    <Modal ref={modalRef} title={title}>
+      <Formik
+        initialValues={initialState}
+        validationSchema={VALIDATION.earningValidation}
+        onSubmit={handleSubmit}
+        validateOnChange={false}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          setFieldValue,
+          values,
+          errors,
+        }) => (
+          <div className="flex flex-col gap-4 ">
+            <TextInput
+              label="Name"
+              onChange={handleChange("name")}
+              onBlur={handleBlur("name")}
+              value={values.name}
+              isError={!!errors.name}
+              message={errors.name}
+            />
+            <TextInput
+              label="Amount"
+              onChange={handleChange("amount")}
+              onBlur={handleBlur("amount")}
+              value={values.amount}
+              isError={!!errors.amount}
+              message={errors.amount}
+            />
+
+            {type === "earning" && (
+              <div className="flex items-center">
+                <input
+                  type="checkbox"
+                  onChange={(e) => {
+                    setFieldValue("isEPFandETP", e.target.checked);
+                  }}
+                  checked={values.isEPFandETP}
+                  className="mr-2"
+                />
+                <span>EPF and ETF</span>
+              </div>
+            )}
+
+            <div className="flex justify-end space-x-2">
+              <Button onClick={() => modalRef.current?.close()}>Cancel</Button>
+              <Button onClick={handleSubmit}>Add</Button>
+            </div>
+          </div>
+        )}
+      </Formik>
+    </Modal>
+  );
+};
