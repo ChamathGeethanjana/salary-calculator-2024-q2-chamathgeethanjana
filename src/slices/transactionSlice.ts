1645e18081bdf8c360d1f008eaecccfa61bd5301
Chamath Geethanjana <geethanjana.cgs@gmail.com>
2024-06-24 00:23:22 +0530

    refactor: Optimize transactionSlice selectors for improved performance

--- src/slices/transactionSlice.ts
+++ src/slices/transactionSlice.ts
@@ -1,6 +1,6 @@
 import { RootState } from "@/store";
 import { Transaction } from "@/types";
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSelector, createSlice } from "@reduxjs/toolkit";
 
 export interface TransactionState {
   transactions: Transaction[];
@@ -46,31 +46,34 @@ export const {
 export const selectTransactions = (state: RootState) =>
   state.transactions.transactions;
 
-export const selectEarnings = (state: RootState) =>
-  state.transactions.transactions.filter(
-    (transaction) => transaction.type === "earning"
-  );
+export const selectEarnings = createSelector(
+  selectTransactions,
+  (transactions) =>
+    transactions.filter((transaction) => transaction.type === "earning")
+);
 
-export const selectDeductions = (state: RootState) =>
-  state.transactions.transactions.filter(
-    (transaction) => transaction.type === "deduction"
-  );
+export const selectDeductions = createSelector(
+  selectTransactions,
+  (transactions) =>
+    transactions.filter((transaction) => transaction.type === "deduction")
+);
 
-export const selectTotalEarnings = (state: RootState) =>
-  state.transactions.transactions
-    .filter((transaction) => transaction.type === "earning")
-    .reduce((acc, transaction) => acc + transaction.amount, 0);
+export const selectTotalEarnings = createSelector(selectEarnings, (earnings) =>
+  earnings.reduce((acc, transaction) => acc + transaction.amount, 0)
+);
 
-export const selectTotalDeductions = (state: RootState) =>
-  state.transactions.transactions
-    .filter((transaction) => transaction.type === "deduction")
-    .reduce((acc, transaction) => acc + transaction.amount, 0);
+export const selectTotalDeductions = createSelector(
+  selectDeductions,
+  (deductions) =>
+    deductions.reduce((acc, transaction) => acc + transaction.amount, 0)
+);
 
-export const selectTotalAllowedEPFandETP = (state: RootState) =>
-  state.transactions.transactions
-    .filter(
-      (transaction) => transaction.type === "earning" && transaction.isEPFandETP
-    )
-    .reduce((acc, transaction) => acc + transaction.amount, 0);
+export const selectTotalAllowedEPFandETP = createSelector(
+  selectEarnings,
+  (earnings) =>
+    earnings
+      .filter((transaction) => transaction.isEPFandETP)
+      .reduce((acc, transaction) => acc + transaction.amount, 0)
+);
 
 export default transactionSlice.reducer;
